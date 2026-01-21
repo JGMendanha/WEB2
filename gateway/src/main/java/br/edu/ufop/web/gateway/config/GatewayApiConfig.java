31f2dc2546f68a1a8cb7f8eefd84067dbfc8abaf
joao.mendanha <joao.mendanha@levty.com>
2026-01-21 20:22:51 -0300

    finish gateway and start frontend

--- gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
+++ gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
@@ -1,5 +1,6 @@
 package br.edu.ufop.web.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -9,15 +10,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayApiConfig {
 
-    private String frontEndUri =  "http://localhost:5173";
+    @Value("${gateway.frontend.uri}")
+    private String frontEndUri;
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 
         return builder.routes()
+                .route("user-api", pred -> pred.path("/api/users/**")
+                        .filters(filter -> filter.rewritePath("/api/users", "/users"))
+                            .uri("lb://users-service"))
                 .route("users",  pred -> pred.path("/users/**").uri("lb://users-service"))
                 .route("sales",  pred -> pred.path("/sales/**").uri("lb://sales-service"))
-                .route("frontend",  pred -> pred.path("/**").uri(this.frontEndUri))
+                .route("frontend",  pred -> pred.path("/**").uri(getFrontEndUri()))
                 .build();
+    }
+
+    private String getFrontEndUri() {
+
+        final String FRONTEND_DEFAULT = "http://localhost:1234";
+
+        if(this.frontEndUri != null) {
+
+            return this.frontEndUri;
+        }
+
+        return FRONTEND_DEFAULT;
     }
 }
